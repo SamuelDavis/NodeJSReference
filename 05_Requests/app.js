b794918fba2d4c1f5e1d7f54e51713582c38486d
Samuel Davis <samueljakdavis@gmail.com>
2013-09-26 13:04:46 -0400

    Added examples for setting response types, redirects, and getting different data from the request.

--- 05_Requests/app.js
+++ 05_Requests/app.js
@@ -15,9 +15,15 @@ app.configure(function() {
 
 /* Define Routes */
 
+/*
+Note: The res.send, res.format, res.redirect, res.json, etc. methods
+can accept a status code as their first argument
+*/
+
 //The root url
 app.get("/", function(req, res) {
-	res.send(JSON.stringify({
+	//Reply to the request using JSON with an explicitly-set content type
+	res.type("text").json(404, {
 		//Get a header(s) from the request object (ex. the user-agent)
 		"User Agent": req.get("user-agent"),
 		//What content types can we respond to the request with
@@ -26,17 +32,34 @@ app.get("/", function(req, res) {
 		"Accepts JSON Response": req.acceptsCharset("JSON"),
 		//What languages can we respond to the quest with
 		"Accepted languages": req.acceptedLanguages
-	}));
+	});
 });
 
-//A custom route which has an optional :name segment
-app.get("/name/:name?", function(req, res) {
-	//grab the :name part of the request, but give it a default value
-	var name = req.param("name", "Default Name");
-	res.send("The name is: " + name);
+//A custom route for responding to different content types
+app.get("/contentTypes", function(req, res) {
+	//Switch the response based on the content type the request accepts
+	res.format({
+		html: function() {
+			res.send("<h1>This response is HTML.</h1>");
+		},
+		json: function() {
+			res.send({
+				message: "This message is JSON."
+			});
+		},
+		text: function() {
+			res.send("This response is text.");
+		}
+	});
+});
+
+//A custom route which redirects the user to the homepage
+app.get("/redirectMe", function(req, res) {
+	//Trigger a redirect to the '/' route
+	res.redirect("/");
 });
 
 //Start the Express server
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
